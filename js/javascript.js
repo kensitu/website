50067e9d79cdf915c819904d4a66a321c51b167b
kensitu <ken@Kens-MBP.wv.cc.cmu.edu>
2016-03-25 04:20:39 -0400

    Changed non-header fonts, added responsive nav scroll, modified footer

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){       
+   $(document).scroll(function() {
+
+      // Checking where the scroll is
+
+      var scrollTop = $(this).scrollTop();
+      var scrollBot = scrollTop + $(window).height();
+      if(scrollBot > ($('#portfolio').offset().top)) {
+          // Designs and Resume is selected
+          $('#DesignsLink').css('background-color', '#393939');
+          $('#ResumeLink').css('background-color', '#393939');
+          $('#ProjectsLink').css('background-color', 'transparent');
+          $('#HomeLink').css('background-color', 'transparent');
+      } else if(scrollTop > ($('#ProjectsText').offset().top) * 2 / 3) {
+          // Projects is selected
+          $('#ProjectsLink').css('background-color', '#393939');
+          $('#HomeLink').css('background-color', 'transparent');
+          $('#DesignsLink').css('background-color', 'transparent');
+          $('#ResumeLink').css('background-color', 'transparent');
+       } else {
+          // home is selected
+          $('#HomeLink').css('background-color', '#393939');
+          $('#ProjectsLink').css('background-color', 'transparent');
+          $('#DesignsLink').css('background-color', 'transparent');
+          $('#ResumeLink').css('background-color', 'transparent');
+       }
+   });
+});
